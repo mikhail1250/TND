43d6974cc68ff3d65cd360fb778a8a3b98b136bc
mikhail1250 <mikhail.ae23@gmail.com>
2026-09-19 16:54:32 +0300

    Add Yandex Metrica tracking (#3)
    Co-authored-by: mikhail1250 <215715558+mikhail1250@users.noreply.github.com>

---
+++ yandex-metrika.js
@@ -0,0 +1,20 @@
+(() => {
+  const counterId = 112816579;
+
+  window.ym = window.ym || function ym() {
+    (window.ym.a = window.ym.a || []).push(arguments);
+  };
+  window.ym.l = Date.now();
+
+  const tag = document.createElement("script");
+  tag.async = true;
+  tag.src = "https://mc.yandex.ru/metrika/tag.js";
+  document.head.appendChild(tag);
+
+  window.ym(counterId, "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: false,
+  });
+})();
